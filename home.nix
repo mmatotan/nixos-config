428e704340a34a8317a60ce15d77ef9d1977e35e
mmatotan <mrkanic@gmail.com>
2025-10-01 16:42:40 +0200

    Hyprland config in home manager

--- home.nix
+++ home.nix
@@ -4,6 +4,7 @@
   home.username = "marko";
   home.homeDirectory = "/home/marko";
   home.stateVersion = "25.05";
+
   programs.bash = {
     enable = true;
   };
@@ -18,6 +19,46 @@
     userEmail = "mrkanic@gmail.com";
     extraConfig = {
       init.defaultBranch = "master";
+    };
+  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      "$mod" = "SUPER";
+      exec = "hyprpaper";
+      monitor = "main, 1920x1080@60.00HHz, 0x0, 1";
+      decoration = {
+        rounding = "4";
+        inactive_opacity = "0.8";
+      };
+      general = {
+        gaps_in = "8";
+        gaps_out = "16";
+        border_size = "4";
+      };
+      input = {
+        follow_mouse = "0";
+        mouse_refocus = "false";
+        accel_profile = "flat";
+      };
+      bind =
+        [
+          "$mod, Return, exec, alacritty"
+          "$mod SHIFT, Return, exec, firefox"
+	  "$mod, q, killactive"
+	  "$mod, f, fullscreen"
+        ]
+        ++ (
+          builtins.concatLists (builtins.genList (i:
+              let ws = i + 1;
+              in [
+                "$mod, code:1${toString i}, workspace, ${toString ws}"
+                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+              ]
+            )
+            9)
+        );
     };
   };
 }
